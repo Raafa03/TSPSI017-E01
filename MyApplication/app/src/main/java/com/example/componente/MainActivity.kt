8e276002c47325fe3d9321f558fd95e0a5e91d3c
Rafael Estevam <rafael2003.estevam@gmail.com>
2023-10-19 20:30:01 +0100

    Componente final

--- MyApplication/app/src/main/java/com/example/componente/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/componente/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.componente
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.componente.ui.theme.ComponenteTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,11 +35,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    Row {
+        Column {
+            Text(text = "Rafael Estevam")
+            Text(text = "19/10/2023")
+        }
+        Image(
+            painter = painterResource(id = R.drawable.unnamed),
+            contentDescription = "Logotipo")
+        
+    }
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
